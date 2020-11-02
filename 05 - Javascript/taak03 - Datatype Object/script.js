d1e87a9a2e552b5403488333b64580ad41ffea15
Joey janmaat <Joey.janmaat@student.rocva.nl>
2020-11-02 15:51:45 +0100

    object gemaakt

---
+++ 05 - Javascript/taak03 - Datatype Object/script.js
@@ -0,0 +1,12 @@
+let student = {
+ mijnvoornaam : "joey",
+ achternaam : "janmaat",
+ leeftijd : "18 jaar",
+ ikBenStemGerechtig : "true",
+ ikMagLegaalAlcoholKopen : "true",
+ ikMagBrommerRijden : "false",
+}
+
+console.log(student);
+console.log(student.leeftijd);
+
