ceedab485ff8b42c099d7890cd7d7a5e6e8ffad2
Joey janmaat <Joey.janmaat@student.rocva.nl>
2020-11-02 17:01:24 +0100

    javascript gemaakt

---
+++ 05 - Javascript/taak08 - To let or not to let/script.js
@@ -0,0 +1,9 @@
+let codingiscool = 10;
+codingiscooler = codingiscool + codingiscool;
+console.log (codingiscool)
+
+let huidigjaartal = 2020;
+let geboortejaar = 2001;
+let mijnleeftijd = 19;
+mijnleeftijd = huidigjaartal - geboortejaar;
+console.log (mijnleeftijd);
