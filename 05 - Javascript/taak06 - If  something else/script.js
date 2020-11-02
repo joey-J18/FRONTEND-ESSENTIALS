802c1c2dfa0893dcfcf5e6bc80fc5ef89e7dfbc7
Joey janmaat <Joey.janmaat@student.rocva.nl>
2020-11-02 16:48:00 +0100

    kleur veranderd met js

---
+++ 05 - Javascript/taak06 - If  something else/script.js
@@ -0,0 +1,9 @@
+let stopLichtKleur = rood;
+
+
+if(stopLichtKleur == rood){
+    alert("jij moet stoppen voor het stoplicht")
+}
+else{
+    alert("anders mag je doorrijden");
+}
--- 05 - Javascript/taak07 - Intelligente DOM/website/script.js
+++ 05 - Javascript/taak07 - Intelligente DOM/website/script.js
@@ -7,3 +7,11 @@ console.log(window.document);
 
 
 // document.querySelector("#poster1");
+document.querySelector("#poster1")
+.style.backgroundColor = '#eeffaa';
+
+document.querySelector("#poster2")
+.style.backgroundColor = '#0000FF';
+
+document.querySelector("#poster3")
+.style.backgroundColor = '#BDBDBD';
