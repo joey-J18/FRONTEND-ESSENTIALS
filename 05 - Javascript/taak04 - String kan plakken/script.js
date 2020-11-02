bf4ab4f2c90dc8a5f57d7d270a5c684b0d18c522
Joey janmaat <Joey.janmaat@student.rocva.nl>
2020-11-02 16:01:36 +0100

    string kan plakken

---
+++ 05 - Javascript/taak04 - String kan plakken/script.js
@@ -0,0 +1,6 @@
+let mijnVoornaam = "joey ";
+let mijnAchternaam = 'janmaat ';
+let mijnleeftijd = '18 ';
+
+let ikstelmevoor = "mijn naam is " + mijnVoornaam + mijnAchternaam + "en ik ben " + mijnleeftijd + "jaar oud";
+console.log(ikstelmevoor);
