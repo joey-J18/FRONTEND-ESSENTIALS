4e7dff96f8ff8d7b3d597553e91d3f50ecdd7520
Joey janmaat <Joey.janmaat@student.rocva.nl>
2020-11-02 15:43:26 +0100

    console log gemaakt

---
+++ 05 - Javascript/taak02 - Op welke type let jij/script.js
@@ -0,0 +1,15 @@
+let mijnvoornaam = "joey";
+let achternaam = "janmaat";
+let leeftijd = "18 jaar";
+let ikBenStemGerechtig ="true"
+let ikMagLegaalAlcoholKopen="true"
+let ikMagBrommerRijden="false"
+let SoftwareOntwikkeling="true"
+
+console.log(mijnvoornaam)
+console.log(achternaam)
+console.log(leeftijd)
+console.log(ikBenStemGerechtig)
+console.log(ikMagLegaalAlcoholKopen)
+console.log(ikMagBrommerRijden)
+console.log(SoftwareOntwikkeling)
