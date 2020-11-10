8d9964dbd3e827cb6fc955de8db17e88cbd65a59
Joey janmaat <Joey.janmaat@student.rocva.nl>
2020-11-10 11:35:21 +0100

    stukje js erbij

--- opdracht/Eind-opdracht/js/script.js
+++ opdracht/Eind-opdracht/js/script.js
@@ -1,37 +1,34 @@
-
-document.querySelector("#Voornaam").innerHTML =prompt()
-
-
-
-
-document.querySelector("#Achternaam").innerHTML =prompt()
-
-
-
-
-document.querySelector("#Email").innerHTML =prompt()
+var bezoeker = {
+    voornaam : prompt("Wat is je voornaam"),
+    achternaam : prompt("Wat is je achternaam"),
+    email : prompt("Wat is je email"),
+    woonplaats : prompt("Wat is je woonplaats"),
+    leeftijd : prompt("wat is je leeftijd"),
+};
 
 
 
 
-document.querySelector("#Woonplaats").innerHTML =prompt()
+document.querySelector("#Voornaam").innerHTML = (bezoeker.voornaam)
+document.querySelector("#Achternaam").innerHTML = (bezoeker.achternaam)
+document.querySelector("#Email").innerHTML = (bezoeker.email)
+document.querySelector("#Woonplaats").innerHTML = (bezoeker.woonplaats)
+document.querySelector("#Leeftijd").innerHTML = (bezoeker.leeftijd)
 
 
-var leeftijd = prompt("wat is je leeftijd");
-document.getElementById("Leeftijd").innerHTML = leeftijd
-if (leeftijd == 16){
+if (bezoeker.leeftijd == 16){
     document.body.style.backgroundColor = "orange";
 }
-if (leeftijd == 17){
+if (bezoeker.leeftijd == 17){
     document.body.style.backgroundColor = "blue";
 }
-if (leeftijd == 18){
+if (bezoeker.leeftijd == 18){
     document.body.style.backgroundColor = "gold";
 }
-if (leeftijd == 19){
+if (bezoeker.leeftijd == 19){
     document.body.style.backgroundColor = "green";
 }
-if (leeftijd == niks){
+if (bezoeker.leeftijd == null){
     document.body.style.backgroundColor = "purple";
 };
 
