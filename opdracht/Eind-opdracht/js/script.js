2df48ae02b93ba669af64be4310a08d322cf48a6
Joey janmaat <Joey.janmaat@student.rocva.nl>
2020-11-10 10:44:49 +0100

    eindopdracht2

---
+++ opdracht/Eind-opdracht/js/script.js
@@ -0,0 +1,44 @@
+
+document.querySelector("#Voornaam").innerHTML =prompt()
+
+
+
+
+document.querySelector("#Achternaam").innerHTML =prompt()
+
+
+
+
+document.querySelector("#Email").innerHTML =prompt()
+
+
+
+
+document.querySelector("#Woonplaats").innerHTML =prompt()
+
+
+var leeftijd = prompt("wat is je leeftijd");
+document.getElementById("Leeftijd").innerHTML = leeftijd
+if (leeftijd == 16){
+    document.body.style.backgroundColor = "orange";
+}
+if (leeftijd == 17){
+    document.body.style.backgroundColor = "blue";
+}
+if (leeftijd == 18){
+    document.body.style.backgroundColor = "gold";
+}
+if (leeftijd == 19){
+    document.body.style.backgroundColor = "green";
+}
+if (leeftijd == niks){
+    document.body.style.backgroundColor = "purple";
+};
+
+
+
+
+
+
+
+
